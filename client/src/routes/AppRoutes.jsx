57be3e87ffba035c566cde97c5da76ba2c49f8b7
AjayBadhe <ajaybadhe2024@gmail.com>
2026-08-07 19:58:14 +0530

    Setup of project

---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+import Dashboard from "../pages/Dashboard/Dashboard";
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;
